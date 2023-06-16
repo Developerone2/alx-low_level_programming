11488b66547d20d62d13cd9264a3da472fb719ff
Developerone2 <Rebeccaeyob44@gmail.com>
2023-06-16 07:42:30 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry.
+ * return: (1)
+ * Description - Print a text
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-18-19\n", 59);
+	return (1);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %u byte(s)\n", size of (char));
-printf("Size of an int: %u byte(s)\n", size of(int));
-printf("Size of a long int: %u byte(s)\n", size of(long int));
-printf("Size of a long long int: %u byte(s)\n", size of(long long int));
-printf("Size of a float ; %u byte(s)\n", size of(float));
+printf("Size of a char: %i byte(s)\n", size of (char));
+printf("Size of an int: %i byte(s)\n", size of(int));
+printf("Size of a long int: %i byte(s)\n", size of(long int));
+printf("Size of a long long int: %i byte(s)\n", size of(long long int));
+printf("Size of a float ; %i byte(s)\n", size of(float));
 return (0);
 }
