a7fe61299f82da4ff4c35699b5aa4f3b27fb1cb6
Developerone2 <Rebeccaeyob44@gmail.com>
2023-06-16 08:27:45 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,7 @@
  * main - Entry point
  * Description: print a quote using write function
  * return: 1
+ */
 int main(void)
 {
 	write 1,( "and that piece of art is useful\" - Dora Korpar, 2015-18-19\n", 59);
