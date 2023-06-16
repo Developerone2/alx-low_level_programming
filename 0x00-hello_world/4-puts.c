2ae918e53bcae81fb7d4b7777e9a7f80d3de8ad1
Developerone2 <Rebeccaeyob44@gmail.com>
2023-06-16 06:58:53 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
 * main - A program that prints line with puts function
-* Return: 0 (success) 
+* Return: 0 (success)
+* Description - Print a text 
 */
 int main(void) 
 {
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)", size of(char));
-printf("Size of an int: %d byte(s)", size of(int));
-printf("Size of a long int: %d byte(s)", size of(long int));
-printf("Size of a long long int: %d byte(s)", size of(long long int));
-printf("Size of a float ; %d byte(s)", size of(float));
+printf("Size of a char: %u byte(s)\n", size of (char));
+printf("Size of an int: %u byte(s)\n", size of(int));
+printf("Size of a long int: %u byte(s)\n", size of(long int));
+printf("Size of a long long int: %u byte(s)\n", size of(long long int));
+printf("Size of a float ; %u byte(s)\n", size of(float));
 return (0);
 }
