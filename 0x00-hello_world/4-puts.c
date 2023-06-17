0213f4f2569916101dfeb4f4b65bfd4fd9c82179
Developerone2 <Rebeccaeyob44@gmail.com>
2023-06-17 13:37:18 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
 * main - A program that prints line with puts function
 * Return: 0 (success)
-* Description - Print a text 
+* Description - Print a text
 */
-int main(void) 
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
