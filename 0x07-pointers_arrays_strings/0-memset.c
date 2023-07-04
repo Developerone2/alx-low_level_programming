825b4130aa89cd9ae053b54fc082968ad7195f5f
Developerone2 <Rebeccaeyob44@gmail.com>
2023-07-03 17:42:17 -0700

    memcpy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* *_memset - fills memory with a constant byte
+* _memset - fills memory with a constant byte
 * @s: pointer to put the constant
 * @b: constant
 * @n: max bytes to use
@@ -17,5 +17,5 @@ for (itr = 0; n > 0; itr++, n--)
 s[itr] = b;
 }
 
-return (0);
+return (s);
 }
