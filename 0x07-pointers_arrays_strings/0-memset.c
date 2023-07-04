4c233216299c02e7ba5b86feaec6946042a4eda3
Developerone2 <Rebeccaeyob44@gmail.com>
2023-07-03 19:09:28 -0700

    chess

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
-#include "main.h"
+include "main.h"
 
 /**
-* _memset - fills memory with a constant byte
-* @s: pointer to put the constant
-* @b: constant
-* @n: max bytes to use
-* Return: pointer s
-*/
-
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int itr;
+unsigned int i;
 
-for (itr = 0; n > 0; itr++, n--)
+for (i = 0; i < n; i++)
 {
-s[itr] = b;
+s[i] = b;
 }
 
 return (s);
